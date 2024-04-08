c1a315985afe2035d7a357a6107ce87b672dd9ea
mikerodionov <13007691+mikerodionov@users.noreply.github.com>
2024-04-08 19:15:29 +0200

    To adjust module outputs references

--- aws-vpc-deploy-module-tests/fixtures/sandbox-deploy/outputs.tf
+++ aws-vpc-deploy-module-tests/fixtures/sandbox-deploy/outputs.tf
@@ -1,59 +1,59 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       =  module.aws_vpc_deploy_module.aws_vpc.vpc.id
+  value       = module.aws_vpc_deploy_module.vpc_id
 }
 
 output "vpc_name" {
   description = "The name tag of the VPC"
-  value       = module.aws_vpc_deploy_module.aws_vpc.vpc.tags["Name"]
+  value       = module.aws_vpc_deploy_module.vpc_name
 }
 
 output "internet_gateway_id" {
   description = "The ID of the Internet Gateway"
-  value       = module.aws_vpc_deploy_module.aws_internet_gateway.igw.id
+  value       = module.aws_vpc_deploy_module.internet_gateway_id
 }
 
 output "internet_gateway_name" {
   description = "The name tag of the Internet Gateway"
-  value       = module.aws_vpc_deploy_module.aws_internet_gateway.igw.tags["Name"]
+  value       = module.aws_vpc_deploy_module.internet_gateway_name
 }
 
 output "public_subnet_ids" {
   description = "The IDs of the public subnets"
-  value       = module.aws_vpc_deploy_module.aws_subnet.public_subnet.*.id
+  value       = module.aws_vpc_deploy_module.public_subnet_ids
 }
 
 output "public_subnet_names" {
   description = "The name tags of the public subnets"
-  value       = [for s in module.aws_vpc_deploy_module.aws_subnet.public_subnet : s.tags["Name"]]
+  value       = module.aws_vpc_deploy_module.public_subnet_names
 }
 
 output "private_subnet_ids" {
   description = "The IDs of the private subnets"
-  value       = module.aws_vpc_deploy_module.aws_subnet.private_subnet.*.id
+  value       = module.aws_vpc_deploy_module.private_subnet_ids
 }
 
 output "private_subnet_names" {
   description = "The name tags of the private subnets"
-  value       = [for s in module.aws_vpc_deploy_module.aws_subnet.private_subnet : s.tags["Name"]]
+  value       = module.aws_vpc_deploy_module.private_subnet_names
 }
 
 output "nat_gateway_ids" {
   description = "The IDs of the NAT Gateways"
-  value       = module.aws_vpc_deploy_module.aws_nat_gateway.nat_gateway.*.id
+  value       = module.aws_vpc_deploy_module.nat_gateway_ids
 }
 
 output "nat_gateway_names" {
   description = "The name tags of the NAT Gateways"
-  value       = [for g in aws_vpc_deploy_module.aws_nat_gateway.nat_gateway : g.tags["Name"]]
+  value       = module.aws_vpc_deploy_module.nat_gateway_names
 }
 
 output "private_route_table_ids" {
   description = "The IDs of the route tables for private subnets"
-  value       = module.aws_vpc_deploy_module.aws_route_table.private_route_table.*.id
+  value       = module.aws_vpc_deploy_module.private_route_table_ids
 }
 
 output "private_route_table_names" {
   description = "The name tags of the route tables for private subnets"
-  value       = [for r in module.aws_vpc_deploy_module.aws_route_table.private_route_table : r.tags["Name"]]
+  value       = module.aws_vpc_deploy_module.private_route_table_names
 }
