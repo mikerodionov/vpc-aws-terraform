e2d4a616c83d370112dbae148d147add32587246
mikerodionov <13007691+mikerodionov@users.noreply.github.com>
2024-04-08 16:02:42 +0200

    To remove awsRegion var

--- aws-vpc-deploy-module-tests/aws-vpc-deploy-module_test.go
+++ aws-vpc-deploy-module-tests/aws-vpc-deploy-module_test.go
@@ -8,27 +8,21 @@ import (
 )
 
 func TestMyTerraformModule(t *testing.T) {
-	awsRegion := os.Getenv("AWS_DEFAULT_REGION")
 	awsAccessKey := os.Getenv("AWS_ACCESS_KEY_ID")
 	awsSecretKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
 
 	// Ensure the variables are not empty
-	if awsRegion == "" || awsAccessKey == "" || awsSecretKey == "" {
+	if awsAccessKey == "" || awsSecretKey == "" {
 		t.Fatal("AWS environment variables must be set")
 	}
 
 	terraformOptions := &terraform.Options{
 		// The path to where your Terraform code is located
 		TerraformDir: "./fixtures/sandbox-deploy/",
-		// Variables to pass to our Terraform code using -var options
-		Vars: map[string]interface{}{
-			"aws_region": awsRegion,
-		},
 		// Environment variables to set when running Terraform
 		EnvVars: map[string]string{
 			"AWS_ACCESS_KEY_ID":     awsAccessKey,
 			"AWS_SECRET_ACCESS_KEY": awsSecretKey,
-			"AWS_DEFAULT_REGION":    awsRegion,
 		},
 	}
 
